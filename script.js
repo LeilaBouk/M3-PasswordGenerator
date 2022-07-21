0d6f1721a182a15ab0886e066efa0486a062d73a
Leila <leila.boukella@gmail.com>
2022-07-20 21:58:57 -0700

    added more prompts, added button functionality

--- script.js
+++ script.js
@@ -19,35 +19,60 @@ function promptUser() {
   //Stop user from entering pasword above 128
   if (passLength > 128) {
     window.alert ('Please choose a smaller number. 128 is the maximum length. 😵‍💫');
-    promptUser();
+    return;
   }
 
   //stop user from entering password below 8
   if (passLength < 8) {
     window.alert ('Please choose a bigger number. 8 is the minimum length. 😵‍💫');
-    promptUser();
+    return;
   }
 
   //Stop user from entering a value that isn't a number
   if (isNaN(passLength)) {
     window.alert ('Please enter a number 🤓');
-    promptUser();
+    return;
   }
 
-  //
  else {
+  passLength = passLength;
   window.alert ('Your selected length is ' + passLength);
+ }
+
+ console.log(passLength);
+
+ //Password Length Determined
+
+ //User parameter questions
+
+ var lowCase = window.confirm('Include lowercase letters?');
+ if (lowCase = true) {
+  console.log("yes to lowcase");
+ }
+
+ var upCase = window.confirm('Include uppercase letters?');
+ if (upCase = true) {
+  console.log("yes to upper");
+ }
+
+ var nums = window.confirm('Include numbers?');
+ if (nums = true) {
+  console.log("yes to numbers");
+ }
+
+ var specials = window.confirm('Include special characters?');
+ if (specials = true) {
+  console.log("yes to special");
  }
   
 }
-
-promptUser();
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  promptUser();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
